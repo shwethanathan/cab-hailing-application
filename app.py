2761e24d6ac4dadfa846275d3b822b20fa790e1a
shwethanathan <126972707+shwethanathan@users.noreply.github.com>
2023-08-18 18:24:41 +0530

    Update app.py
    Working fast api application

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-
+#----------------------------IMPORTS--------------------------------
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import pandas as pd
 from collections import OrderedDict
+from fastapi.staticfiles import StaticFiles
 
 #----------------------VARIABLE DECLARATIONS------------------------
 
@@ -33,40 +34,37 @@ speed= 40 #kmph
 
 #Get Cab data
 cabs=pd.read_excel("/Users/sshwetha/Downloads/cab_data (1).xlsx")
-
 cabs["Location"] = cabs["Location"].apply(lambda x: x.upper())
 cabs["Vehicle"] = cabs["Vehicle"].apply(lambda x: x.lower())
 
 
-#------------------------------------------------------------------
+#----------------------------FUNCTION DEFINITIONS-------------------
     
-#Function to find shortest distance of pick up point to every other node, returns distances, paths
+#Function to find shortest distance of pick up point to every other node, returns distances, paths dictionaries
 def shortest_distance(locations, adj_matrix, pickup):
     
     #Numbering the locations for ease, we use nodes in djikstra and our output will be in nodes
     nodes=[i for i in range(len(locations))]
     source=locations.index(pickup)
     
-    #Setting the variables
+    #Initiliaizing the variables
     distances={ node: float("inf") for node in nodes }
     paths={node:[] for node in nodes}
     
     distances[source]=0
     paths[source].append(source) 
     
-    
+    #Djikstra's algorithm
     def djikstra(adj_matrix, source, unvisited, distances, paths):
         
         #Removing source from unvisited
         if source in unvisited:
             unvisited.remove(source)
         
-        
         #Base case
         if not unvisited :
             return distances,paths
-            
-            
+                 
         #For finding the next source node, i.e closest adjacent node
         next_source_dist=float("inf")
         next_source_node= None
@@ -75,8 +73,6 @@ def shortest_distance(locations, adj_matrix, pickup):
         for node in unvisited:
             if adj_matrix[source][node]!=0:
                 
-                
-                #print("checking: ",node)
                 #if new path from source is shorter than previous
                 if distances[source] + adj_matrix[source][node]  < distances[node]:
                     #update distance
@@ -84,27 +80,33 @@ def shortest_distance(locations, adj_matrix, pickup):
                     #update path
                     paths[node] = paths[source] +[node]
                 
-                #to find closest adjacent node   
+                #for finding closest adjacent node   
                 if adj_matrix[source][node]<next_source_dist:
                     next_source_node=node
                     next_source_dist=adj_matrix[source][node]
         
-        #to trace back if reached and end 
+        #to trace back if reached a dead end or adjacent unvisited nodes not available
         if next_source_node is None:
             next_source_node=paths[source][-2]
             
         return djikstra(adj_matrix,next_source_node,unvisited,distances,paths)
     
+    #Calling djikstra
     distances, paths= djikstra(adj_matrix, source, nodes, distances,paths)
-    #Converting the nodes back to locations in our output
+    
+    #Converting the nodes back to locations in our djikstra output
     op_distances={locations[node]:distance for node,distance in distances.items()}
     op_paths={locations[node]:[locations[i] for i in path] for node,path in paths.items()}
     
     return op_distances, op_paths
 
+
+
 #Function to calculate travel time, returns time in minutes 
 def time(dist, speed):
     return dist*60/speed
+
+
 
 #Function to calculate fare, returns fare
 def fare_calculation(rates, base_dist, base_charges, dist, vehicle):
@@ -113,7 +115,9 @@ def fare_calculation(rates, base_dist, base_charges, dist, vehicle):
     else:
         return base_charges[vehicle] + (dist-base_dist)*rates[vehicle]
 
-#Function to allocate cab given cab data, returns Cab ID
+
+
+#Function to allocate cab given cab data, returns Driver name, Plate no., Cab Location
 def allocation(cabs, distances, vehicle, drop):
     
     #to sort the distances
@@ -134,33 +138,32 @@ def allocation(cabs, distances, vehicle, drop):
             ID = cabs[cabs["Trip Count"] == cabs["Trip Count"].min()].index[0]
             break
     
-    #If no cab found
-    if ID==None:
-        return None
-    
     #If cab found
     if ID!=None:
-        cab_location=cabs.loc[ID, "Location"]
-        
-        #Update cab info
-        cabs.loc[ID, "Trip Count"]+=1
-        cabs.loc[ID, "Location"]= drop
-            
-        return cabs.loc[ID, "Driver"],cabs.loc[ID, "Plate no."], cab_location
+        return cabs.loc[ID, "Driver"],cabs.loc[ID, "Plate no."],cabs.loc[ID, "Location"]
     
-   
-#Removes innaccessible nodes from locations and adj_matrix, return new locations, adj_matrix
+       
+    #If no cab found
+    else:
+        return None
+
+
+
+#Removes innaccessible nodes from locations and adj_matrix, returns new locations, new adj_matrix
 def remove_na_location(locations, adj_matrix, na_locations):
     
     for location in na_locations:
-        
-        adj_matrix.pop(locations.index(location)) #removes row
+        adj_matrix.pop(locations.index(location)) #removes from row in adj_matrix
         for row in adj_matrix:
-            row.pop(location.index(location))
+            row.pop(location.index(location)) #removes from columns in adji_matrix
         locations.remove(location) #removes from locations
     
     return adj_matrix, locations
 
+
+
+
+#To get complete final result, returns dictionary of values to be passed into html response
 def get_result(
     request: Request,
     pickup: str = Form(...),
@@ -182,9 +185,9 @@ def get_result(
     #Gets distances and paths to every node from pickup
     distances, paths = shortest_distance(locations, adj_matrix, pickup)
 
-
-    #Allocate Cab
-    if allocation(cabs, distances, vehicle, drop)!=None:
+    
+    try:
+        #Allocate cab
         driver, plate, cab_location = allocation(cabs, distances, vehicle, drop)
 
         #Calculate waiting time
@@ -202,40 +205,36 @@ def get_result(
         #Calculate Fare
         fare=fare_calculation(rates, base_dist, base_charges, trip_dist, vehicle)
         
-       
-    
-
-        
-        return templates.TemplateResponse(
-            "booking-result.html",
-            {"request": request, "driver": driver, "plate": plate,
+        return {"request": request, "driver": driver, "plate": plate,
              "cab_location": cab_location, "waiting": waiting,
              "trip_path": trip_path, "trip_distance": trip_dist,
-             "trip_time": trip_time, "fare": fare}
-        )
+             "trip_time": trip_time, "fare": fare,"inaccess":" ".join(na_locations)}
+        
+    #If no cab was allocated
+    except:
+         return None
 
-    else:
-        return 'No cab found'
 
 
-
-#main
-
+#------------------------------MAIN--------------------------------
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/Users/sshwetha/Desktop/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
-    return templates.TemplateResponse("booking-form.html", {"request": request})
+    return templates.TemplateResponse("booking-form.html", {"request": request, "inaccess": " ".join(na_locations)})
 
 @app.post("/cab-hailing/")
 def show_result( request: Request, pickup: str = Form(...),  drop: str = Form(...), vehicle: str = Form(...) ):
+    
     result =get_result(
         request,
-        pickup,
-        drop,
+        pickup.upper(),
+        drop.upper(),
         vehicle,
         locations=locations,
         adj_matrix=adj_matrix,
@@ -247,7 +246,12 @@ def show_result( request: Request, pickup: str = Form(...),  drop: str = Form(..
         cabs=cabs
     )
     
-    return result
+    if result != None:
+        return templates.TemplateResponse("booking-result.html", result)
+    
+    else:
+        return "No cab found"
+
 
     
 
